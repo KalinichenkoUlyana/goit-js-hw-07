53f514cb0af7fc7f11aee95a293b6d0f580eae4f
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-08-04 00:44:25 +0300

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const imageMarkup = createaImagesGallery(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', imageMarkup);
+
+function createaImagesGallery(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return`<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+        }).join('');
+}
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" >
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
